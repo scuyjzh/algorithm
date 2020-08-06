89b030a75c74be238dc2ba3d7b4c864edf9cd501
scuyjzh <scuyjzh@163.com>
2020-08-06 10:38:55 +0800

    update

--- src/com/scuyjzh/sort/QuickSort.java
+++ src/com/scuyjzh/sort/QuickSort.java
@@ -5,52 +5,60 @@ package com.scuyjzh.sort;
  * @date 2020/7/8 16:12
  */
 class QuickSort {
-    public void sort(int[] arr, int left, int right) {
-        if (left < right) {
-            // 获取枢纽值，并将其放在当前待处理序列末尾
-            dealPivot(arr, left, right);
-            // 枢纽值被放在序列末尾
-            int pivot = right - 1;
-            // 左指针
-            int i = left;
-            // 右指针
-            int j = right - 1;
-            while (true) {
-                while (arr[++i] < arr[pivot]) {
-                }
-                while (j > left && arr[--j] > arr[pivot]) {
-                }
-                if (i < j) {
-                    swap(arr, i, j);
-                } else {
-                    break;
-                }
-            }
-            if (i < right) {
-                swap(arr, i, right - 1);
-            }
-            sort(arr, left, i - 1);
-            sort(arr, i + 1, right);
+    /**
+     * 快速排序
+     *
+     * @param array
+     */
+    public void quickSort(int[] array) {
+        int len;
+        if (array == null
+                || (len = array.length) == 0
+                || len == 1) {
+            return;
         }
+        sort(array, 0, len - 1);
     }
 
-    private void dealPivot(int[] arr, int left, int right) {
-        int mid = (left + right) / 2;
-        if (arr[left] > arr[mid]) {
-            swap(arr, left, mid);
+    /**
+     * 快排核心算法，递归实现
+     *
+     * @param array
+     * @param left
+     * @param right
+     */
+    public void sort(int[] array, int left, int right) {
+        if (left > right) {
+            return;
         }
-        if (arr[left] > arr[right]) {
-            swap(arr, left, right);
-        }
-        if (arr[right] < arr[mid]) {
-            swap(arr, right, mid);
-        }
-        swap(arr, right - 1, mid);
-    }
 
-    private void swap(int[] arr, int a, int b) {
-        int temp = arr[a];
-        arr[a] = arr[b];
-        arr[b] = temp;
+        // base中存放基准数
+        int base = array[left];
+        int i = left, j = right;
+        while (i != j) {
+            // 顺序很重要，先从右边开始往左找，直到找到比base值小的数
+            while (array[j] >= base && i < j) {
+                j--;
+            }
+            // 再从左往右边找，直到找到比base值大的数
+            while (array[i] <= base && i < j) {
+                i++;
+            }
+            // 上面的循环结束表示找到了位置或者(i>=j)了，交换两个数在数组中的位置
+            if (i < j) {
+                int tmp = array[i];
+                array[i] = array[j];
+                array[j] = tmp;
+            }
+        }
+
+        // 将基准数放到中间的位置（基准数归位）
+        array[left] = array[i];
+        array[i] = base;
+
+        // 递归，继续向基准的左右两边执行和上面同样的操作
+        // i的索引处为上面已确定好的基准值的位置，无需再处理
+        sort(array, left, i - 1);
+        sort(array, i + 1, right);
     }
 }
