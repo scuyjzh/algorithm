6dcca25044d96f2b968606380d500efbcaeea107
scuyjzh <scuyjzh@163.com>
2020-08-12 11:27:08 +0800

    update

--- src/com/scuyjzh/search/FibonacciSearch.java
+++ src/com/scuyjzh/search/FibonacciSearch.java
@@ -84,9 +84,9 @@ class FibonacciSearch {
     }
 
     public static void main(String[] args) {
-        int[] arr = new int[]{0, 1, 2, 3, 4, 5, 6, 7};
+        int[] arr = new int[]{1, 2, 3, 4, 5, 6, 7, 8};
         FibonacciSearch fibonacciSearch = new FibonacciSearch();
-        int res = fibonacciSearch.search(arr, 6);
+        int res = fibonacciSearch.search(arr, 7);
         System.out.println(res);
     }
 }
