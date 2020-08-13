155e7fcf8433fc9b30f6ee1743dad691bbfb0be3
scuyjzh <scuyjzh@163.com>
2020-08-14 03:24:17 +0800

    update

--- src/com/scuyjzh/binarytree/LeafNumOnKthLevel.java
+++ src/com/scuyjzh/binarytree/LeafNumOnKthLevel.java
@@ -1,8 +1,80 @@
 package com.scuyjzh.binarytree;
 
+import java.util.*;
+
 /**
  * @author scuyjzh
  * @data 2020/8/14 2:57
  */
 class LeafNumOnKthLevel {
+    /**
+     * Approach #1 (Iteration by level-order traversal - BFS)
+     */
+    public int getLeafNumOnKthLevel1(TreeNode root, int k) {
+        if (root == null) {
+            return 0;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int currLevel = 0;
+        int currLevelNodeTotal = 0;
+        while (!queue.isEmpty()) {
+            currLevel++;
+            currLevelNodeTotal = queue.size();
+            // 如果当前层数等于给定层数，则退出
+            if (currLevel == k) {
+                break;
+            }
+            // 将下一层节点入队
+            int cntNode = 0;
+            while (cntNode < currLevelNodeTotal) {
+                cntNode++;
+                TreeNode curr = queue.poll();
+                if (curr.left != null) {
+                    queue.offer(curr.left);
+                }
+                if (curr.right != null) {
+                    queue.offer(curr.right);
+                }
+            }
+        }
+        int cntNode = 0;
+        int leafNode = 0;
+        while (cntNode < currLevelNodeTotal) {
+            cntNode++;
+            TreeNode curr = queue.poll();
+            if (curr.left == null && curr.right == null) {
+                leafNode++;
+            }
+        }
+        return leafNode;
+    }
+
+    /**
+     * Approach #2 (Recursion - DFS)
+     */
+    public int getLeafNumOnKthLevel2(TreeNode root, int k) {
+        return dfs(root, k);
+    }
+
+    private int dfs(TreeNode root, int k) {
+        if (root == null || k < 1) {
+            return 0;
+        }
+        if (k == 1) {
+            if (root.left == null && root.right == null) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+        return dfs(root.left, k - 1) + dfs(root.right, k - 1);
+    }
+
+    public static void main(String[] args) {
+        LeafNumOnKthLevel solution = new LeafNumOnKthLevel();
+        TreeNode root = TreeNode.initBinaryTree("[3,9,20,null,null,15,7]");
+        System.out.println(solution.getLeafNumOnKthLevel1(root, 3));
+        System.out.println(solution.getLeafNumOnKthLevel2(root, 3));
+    }
 }
