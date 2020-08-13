b3a113e6cc7213d41318a37710835335489ebb23
scuyjzh <scuyjzh@163.com>
2020-08-13 19:45:14 +0800

    update

---
+++ src/com/scuyjzh/binarytree/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,56 @@
+package com.scuyjzh.binarytree;
+
+import java.util.*;
+
+/**
+ * @author scuyjzh
+ * @date 2020/8/13 19:43
+ */
+class MaximumDepthOfBinaryTree {
+    /**
+     * Approach #1 (Iteration by level-order traversal - BFS)
+     */
+    public int maxDepth1(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int depth = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            while (size-- > 0) {
+                TreeNode curr = queue.poll();
+                if (curr.left != null) {
+                    queue.offer(curr.left);
+                }
+                if (curr.right != null) {
+                    queue.offer(curr.right);
+                }
+            }
+            depth++;
+        }
+        return depth;
+    }
+
+    /**
+     * Approach #2 (Recursion - DFS)
+     */
+    public int maxDepth2(TreeNode root) {
+        return dfs(root);
+    }
+
+    private int dfs(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return 1 + Math.max(dfs(root.left), dfs(root.right));
+    }
+
+    public static void main(String[] args) {
+        MaximumDepthOfBinaryTree solution = new MaximumDepthOfBinaryTree();
+        TreeNode root = TreeNode.initBinaryTree("[3,9,20,null,null,15,7]");
+        System.out.println(solution.maxDepth1(root));
+        System.out.println(solution.maxDepth2(root));
+    }
+}
