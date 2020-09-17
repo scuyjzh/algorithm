0823443fcdc7d991cfe537539117b8d6ad5f214f
scuyjzh <scuyjzh@163.com>
2020-09-17 17:51:36 +0800

    update

--- src/com/scuyjzh/design/MyQueue.java
+++ src/com/scuyjzh/design/MyQueue.java
@@ -1,5 +1,7 @@
 package com.scuyjzh.design;
 
+import java.util.*;
+
 /**
  * 使用栈实现队列的下列操作：
  * push(x) -- 将一个元素放入队列的尾部。
@@ -11,4 +13,70 @@ package com.scuyjzh.design;
  * @date 2020/9/17 17:41
  */
 class MyQueue {
+    private Deque<Integer> pushStack;
+    private Deque<Integer> popStack;
+
+    /**
+     * Initialize your data structure here.
+     */
+    public MyQueue() {
+        pushStack = new ArrayDeque<>();
+        popStack = new ArrayDeque<>();
+    }
+
+    /**
+     * Push element x to the back of queue.
+     */
+    public void push(int x) {
+        // 在任何时候都可以向 pushStack 推入元素
+        pushStack.push(x);
+    }
+
+    /**
+     * Removes the element from in front of queue and returns that element.
+     */
+    public int pop() {
+        // 从 popStack 取出元素
+        if (!popStack.isEmpty()) {
+            return popStack.pop();
+        }
+        // 走到这里是因为 popStack 为空，此时需要将 pushStack 里的所有元素依次放入 popStack
+        while (!pushStack.isEmpty()) {
+            popStack.push(pushStack.pop());
+        }
+        return popStack.pop();
+    }
+
+    /**
+     * Get the front element.
+     */
+    public int peek() {
+        // 从 popStack 取出元素
+        if (!popStack.isEmpty()) {
+            return popStack.peek();
+        }
+        // 走到这里是因为 popStack 为空，此时需要将 pushStack 里的所有元素依次放入 popStack
+        while (!pushStack.isEmpty()) {
+            popStack.push(pushStack.pop());
+        }
+        return popStack.peek();
+    }
+
+    /**
+     * Returns whether the queue is empty.
+     */
+    public boolean empty() {
+        // 两个栈都为空，才说明队列为空
+        return pushStack.isEmpty() && popStack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        MyQueue q = new MyQueue();
+        q.push(1);
+        q.push(2);
+        q.push(3);
+        System.out.println(q.pop());
+        System.out.println(q.peek());
+        System.out.println(q.empty());
+    }
 }
