fa0fdce1f6b59c7b308b9ccb31237a2a0b1837fa
scuyjzh <scuyjzh@163.com>
2020-09-17 10:12:53 +0800

    update

--- src/com/scuyjzh/tree/UniqueBinarySearchTrees.java
+++ src/com/scuyjzh/tree/UniqueBinarySearchTrees.java
@@ -1,6 +1,8 @@
 package com.scuyjzh.tree;
 
 /**
+ * 给定一个整数 n，求以 1 ... n 为节点组成的二叉搜索树有多少种？
+ *
  * @author scuyjzh
  * @date 2020/9/17 9:14
  */
@@ -8,7 +10,15 @@ class UniqueBinarySearchTrees {
     /**
      * 方法一：动态规划
      */
-    public int numTrees1(int n) {
+    public int numTrees(int n) {
+        /*
+            定义两个函数：
+            1.G(n): 长度为 n 的序列能构成的不同二叉搜索树的个数。
+            2.F(i,n): 以 i 为根、序列长度为 n 的不同二叉搜索树个数 (1≤i≤n)。
+            不同的二叉搜索树的总数 G(n)，是对遍历所有 i (1≤i≤n) 的 F(i,n) 之和。
+            根为 i 的所有二叉搜索树的集合是左子树集合和右子树集合的笛卡尔积，
+            对于笛卡尔积中的每个元素，加上根节点之后形成完整的二叉搜索树，即F(i,n)=G(i−1)⋅G(n−i)。
+        */
         int[] G = new int[n + 1];
         G[0] = 1;
         G[1] = 1;
@@ -19,5 +29,10 @@ class UniqueBinarySearchTrees {
             }
         }
         return G[n];
+    }
+
+    public static void main(String[] args) {
+        UniqueBinarySearchTrees solution = new UniqueBinarySearchTrees();
+        System.out.println(solution.numTrees(3));
     }
 }
