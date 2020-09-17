30e1bcc94377bfcfbaa62e54646a71ae87d0b617
scuyjzh <scuyjzh@163.com>
2020-09-17 16:24:31 +0800

    update

--- src/com/scuyjzh/tree/TreeNode.java
+++ src/com/scuyjzh/tree/TreeNode.java
@@ -9,8 +9,14 @@ class TreeNode {
     public TreeNode left;
     public TreeNode right;
 
-    public TreeNode(int x) {
-        val = x;
+    public TreeNode(int val) {
+        this.val = val;
+    }
+
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
     }
 
     public static TreeNode initBinaryTree(String str) {
