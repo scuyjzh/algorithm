711c4a7025d83ea19ac6f7e339a6c3cbf842c118
scuyjzh <scuyjzh@163.com>
2020-09-17 18:10:46 +0800

    update

--- src/com/scuyjzh/design/MyStack.java
+++ src/com/scuyjzh/design/MyStack.java
@@ -1,5 +1,7 @@
 package com.scuyjzh.design;
 
+import java.util.*;
+
 /**
  * 使用队列实现栈的下列操作：
  * push(x) -- 元素 x 入栈
@@ -11,4 +13,53 @@ package com.scuyjzh.design;
  * @date 2020/9/17 17:40
  */
 class MyStack {
+    private Deque<Integer> queue;
+
+    /**
+     * Initialize your data structure here.
+     */
+    public MyStack() {
+        queue = new LinkedList<>();
+    }
+
+    /**
+     * Push element x onto stack.
+     */
+    public void push(int x) {
+        queue.push(x);
+        for (int i = 1; i < queue.size(); i++) {
+            queue.push(queue.pop());
+        }
+    }
+
+    /**
+     * Removes the element on top of the stack and returns that element.
+     */
+    public int pop() {
+        return queue.poll();
+    }
+
+    /**
+     * Get the top element.
+     */
+    public int top() {
+        return queue.peek();
+    }
+
+    /**
+     * Returns whether the stack is empty.
+     */
+    public boolean empty() {
+        return queue.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        MyStack s = new MyStack();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        System.out.println(s.pop());
+        System.out.println(s.top());
+        System.out.println(s.empty());
+    }
 }
