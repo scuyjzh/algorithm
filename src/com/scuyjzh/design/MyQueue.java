e820b65f7a6bf5936f8d698ccf0cf65e7d009a2b
scuyjzh <scuyjzh@163.com>
2020-09-17 17:42:33 +0800

    update

---
+++ src/com/scuyjzh/design/MyQueue.java
@@ -0,0 +1,14 @@
+package com.scuyjzh.design;
+
+/**
+ * 使用栈实现队列的下列操作：
+ * push(x) -- 将一个元素放入队列的尾部。
+ * pop() -- 从队列首部移除元素。
+ * peek() -- 返回队列首部的元素。
+ * empty() -- 返回队列是否为空。
+ *
+ * @author scuyjzh
+ * @date 2020/9/17 17:41
+ */
+class MyQueue {
+}
---
+++ src/com/scuyjzh/design/MyStack.java
@@ -0,0 +1,14 @@
+package com.scuyjzh.design;
+
+/**
+ * 使用队列实现栈的下列操作：
+ * push(x) -- 元素 x 入栈
+ * pop() -- 移除栈顶元素
+ * top() -- 获取栈顶元素
+ * empty() -- 返回栈是否为空
+ *
+ * @author scuyjzh
+ * @date 2020/9/17 17:40
+ */
+class MyStack {
+}
