a2833e8cceda36ec9b5cc40463fd3cdcaa6285d8
scuyjzh <scuyjzh@163.com>
2020-09-18 17:31:47 +0800

    update

--- src/com/scuyjzh/tree/ConvertSortedArrayToBinarySearchTree.java
+++ src/com/scuyjzh/tree/ConvertSortedArrayToBinarySearchTree.java
@@ -1,5 +1,31 @@
-package com.scuyjzh.tree;/**
- * @author zhangyijie
+package com.scuyjzh.tree;
+
+/**
+ * 将一个按照升序排列的有序数组，转换为一棵高度平衡二叉搜索树。
+ *
+ * @author scuyjzh
  * @date 2020/9/18 15:24
- */public class ConvertSortedArrayToBinarySearchTree {
+ */
+class ConvertSortedArrayToBinarySearchTree {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return dfs(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode dfs(int[] nums, int lo, int hi) {
+        if (lo > hi) {
+            return null;
+        }
+        // 以升序数组的中间元素作为根节点 root
+        int mid = lo + (hi - lo) / 2;
+        TreeNode root = new TreeNode(nums[mid]);
+        // 递归构建 root 的左子树与右子树
+        root.left = dfs(nums, lo, mid - 1);
+        root.right = dfs(nums, mid + 1, hi);
+        return root;
+    }
+
+    public static void main(String[] args) {
+        ConvertSortedArrayToBinarySearchTree solution = new ConvertSortedArrayToBinarySearchTree();
+        TreeNode.show(solution.sortedArrayToBST(new int[]{1, 2, 3}));
+    }
 }
