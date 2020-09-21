27444dbe360e40cae2f55b835b22d1c93c097ff8
scuyjzh <scuyjzh@163.com>
2020-09-21 15:03:17 +0800

    update

---
+++ src/com/scuyjzh/tree/BalanceBinarySearchTree.java
@@ -0,0 +1,47 @@
+package com.scuyjzh.tree;
+
+import java.util.*;
+
+/**
+ * 给你一棵二叉搜索树，请你返回一棵平衡后的二叉搜索树，新生成的树应该与原来的树有着相同的节点值。
+ *
+ * @author scuyjzh
+ * @date 2020/9/21 9:21
+ */
+class BalanceBinarySearchTree {
+    public TreeNode balanceBST(TreeNode root) {
+        List<Integer> sortList = new LinkedList<>();
+        // 中序遍历构造有序链表
+        inOrder(root, sortList);
+        // 有序链表构造平衡二叉树
+        return buildTree(sortList, 0, sortList.size() - 1);
+    }
+
+    private void inOrder(TreeNode root, List<Integer> sortList) {
+        if (root != null) {
+            inOrder(root.left, sortList);
+            sortList.add(root.val);
+            inOrder(root.right, sortList);
+        }
+    }
+
+    private TreeNode buildTree(List<Integer> sortList, int start, int end) {
+        if (start > end) {
+            return null;
+        }
+        // 中间节点为root
+        int mid = start + (end - start) / 2;
+        TreeNode root = new TreeNode(sortList.get(mid));
+        // 递归构造左右子树
+        root.left = buildTree(sortList, start, mid - 1);
+        root.right = buildTree(sortList, mid + 1, end);
+        // 返回root
+        return root;
+    }
+
+    public static void main(String[] args) {
+        BalanceBinarySearchTree solution = new BalanceBinarySearchTree();
+        TreeNode root = TreeNode.initBinaryTree("[50,40,80,30,45,60,90,10,35,49,70]");
+        System.out.println(solution.balanceBST(root));
+    }
+}
---
+++ src/com/scuyjzh/tree/ConvertBSTToGreaterTree.java
@@ -0,0 +1,61 @@
+package com.scuyjzh.tree;
+
+import java.util.*;
+
+/**
+ * 给定一个二叉搜索树，把它转换成为累加树，使得每个节点的值是原来的节点值加上所有大于它的节点值之和。
+ *
+ * @author scuyjzh
+ * @date 2020/9/21 14:36
+ */
+class ConvertBSTToGreaterTree {
+    int sum = 0;
+
+    /**
+     * Approach #1 (Recursion by Inverse Inorder Traversal)
+     */
+    public TreeNode convertBST1(TreeNode root) {
+        helper(root);
+        return root;
+    }
+
+    private void helper(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        helper(root.right);
+        sum += root.val;
+        root.val = sum;
+        helper(root.left);
+    }
+
+    /**
+     * Approach #2 (Iteration by Inverse Inorder Traversal)
+     */
+    public TreeNode convertBST2(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode cur = root;
+        int sum = 0;
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        while (cur != null || !stack.isEmpty()) {
+            while (cur != null) {
+                stack.push(cur);
+                cur = cur.right;
+            }
+            cur = stack.pop();
+            sum += cur.val;
+            cur.val = sum;
+            cur = cur.left;
+        }
+        return root;
+    }
+
+    public static void main(String[] args) {
+        ConvertBSTToGreaterTree solution = new ConvertBSTToGreaterTree();
+        TreeNode root = TreeNode.initBinaryTree("[5,2,13]");
+        System.out.println(solution.convertBST1(root));
+        System.out.println(solution.convertBST2(root));
+    }
+}
