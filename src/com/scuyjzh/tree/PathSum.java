89bdc4818a57939243c5e1e4bb7939b6bffbe9fe
scuyjzh <scuyjzh@163.com>
2020-09-24 17:48:19 +0800

    update

--- src/com/scuyjzh/tree/PathSum.java
+++ src/com/scuyjzh/tree/PathSum.java
@@ -1,5 +1,7 @@
 package com.scuyjzh.tree;
 
+import java.util.*;
+
 /**
  * 给定一个二叉树和一个目标和，判断该树中是否存在根节点到叶子节点的路径，这条路径上所有节点值相加等于目标和。
  *
@@ -8,9 +10,76 @@ package com.scuyjzh.tree;
  */
 class PathSum {
     /**
-     * Approach #1 (Recursion - DFS)
+     * Approach #1 (Iteration with Queue - BFS)
      */
     public boolean hasPathSum1(TreeNode root, int sum) {
+        if (root == null) {
+            return false;
+        }
+        Queue<TreeNode> queNode = new LinkedList<>();
+        Queue<Integer> queVal = new LinkedList<>();
+        queNode.offer(root);
+        queVal.offer(root.val);
+        while (!queNode.isEmpty()) {
+            TreeNode cur = queNode.poll();
+            int curSum = queVal.poll();
+            if (cur.left == null && cur.right == null) {
+                if (curSum == sum) {
+                    return true;
+                }
+                continue;
+            }
+            if (cur.left != null) {
+                queNode.offer(cur.left);
+                queVal.offer(cur.left.val + curSum);
+            }
+            if (cur.right != null) {
+                queNode.offer(cur.right);
+                queVal.offer(cur.right.val + curSum);
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Approach #2 (Iteration by Postorder Traversal)
+     */
+    public boolean hasPathSum2(TreeNode root, int sum) {
+        if (root == null) {
+            return false;
+        }
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        TreeNode cur = root;
+        TreeNode pre = null;
+        // 记录当前累计的和
+        int curSum = 0;
+        while (cur != null || !stack.isEmpty()) {
+            while (cur != null) {
+                stack.push(cur);
+                curSum += cur.val;
+                cur = cur.left;
+            }
+            cur = stack.peek();
+            if (cur.left == null && cur.right == null && curSum == sum) {
+                return true;
+            }
+            if (cur.right == null || cur.right == pre) {
+                stack.pop();
+                // 减去出栈的值
+                curSum -= cur.val;
+                pre = cur;
+                cur = null;
+            } else {
+                cur = cur.right;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Approach #3 (Recursion - DFS)
+     */
+    public boolean hasPathSum3(TreeNode root, int sum) {
         return dfs(root, sum);
     }
 
@@ -28,7 +97,7 @@ class PathSum {
         PathSum solution = new PathSum();
         TreeNode root = TreeNode.initBinaryTree("[5,4,8,11,null,13,4,7,2,null,null,null,null,null,1,null,null,3]");
         System.out.println(solution.hasPathSum1(root, 18));
-        System.out.println(solution.hasPathSum1(root, 22));
-        System.out.println(solution.hasPathSum1(root, 23));
+        System.out.println(solution.hasPathSum2(root, 18));
+        System.out.println(solution.hasPathSum3(root, 18));
     }
 }
