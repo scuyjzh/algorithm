9b579e85d4a2b69e1af9565cbe67130f08484970
scuyjzh <scuyjzh@163.com>
2020-09-28 01:47:57 +0800

    update

---
+++ src/com/scuyjzh/tree/PopulatingNextRightPointersInEachNodeII.java
@@ -0,0 +1,101 @@
+package com.scuyjzh.tree;
+
+import java.util.*;
+
+/**
+ * 给定一个二叉树，填充它的每个 next 指针，让这个指针指向其下一个右侧节点。如果找不到下一个右侧节点，则将 next 指针设置为 NULL。
+ * 初始状态下，所有 next 指针都被设置为 NULL。
+ *
+ * @author scuyjzh
+ * @date 2020/9/28 1:33
+ */
+class PopulatingNextRightPointersInEachNodeII {
+    /**
+     * Definition for a Node.
+     */
+    class Node {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node next;
+
+        public Node() {
+        }
+
+        public Node(int val) {
+            this.val = val;
+        }
+
+        public Node(int val, Node left, Node right, Node next) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+            this.next = next;
+        }
+    }
+
+    /**
+     * 方法一：层序遍历
+     */
+    public Node connect1(Node root) {
+        if (root == null) {
+            return null;
+        }
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int n = queue.size();
+            Node last = null;
+            for (int i = 1; i <= n; ++i) {
+                Node node = queue.poll();
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+                if (i != 1) {
+                    last.next = node;
+                }
+                last = node;
+            }
+        }
+        return root;
+    }
+
+    Node last = null, nextStart = null;
+
+    /**
+     * 方法二：使用已建立的 next 指针
+     */
+    public Node connect2(Node root) {
+        if (root == null) {
+            return null;
+        }
+        Node start = root;
+        while (start != null) {
+            last = null;
+            nextStart = null;
+            for (Node p = start; p != null; p = p.next) {
+                if (p.left != null) {
+                    handle(p.left);
+                }
+                if (p.right != null) {
+                    handle(p.right);
+                }
+            }
+            start = nextStart;
+        }
+        return root;
+    }
+
+    private void handle(Node p) {
+        if (last != null) {
+            last.next = p;
+        }
+        if (nextStart == null) {
+            nextStart = p;
+        }
+        last = p;
+    }
+}
