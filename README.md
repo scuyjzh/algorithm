4ac1da6e11fdbd4d4020cc5b8091888a40cd25a5
scuyjzh <scuyjzh@163.com>
2021-08-29 01:47:09 +0800

    modify solutions to algorithm - "Bucket sort"

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # algorithm
+
 ✏️Data Structures and Algorithms in JAVA.
+
+## Table of Contents
+
+- [Sort](#Sort)
+- [Search](#Search)
+
+### Sort
+
+| 算法     | 题解                                                        | 时间复杂度        | 空间复杂度   |
+| -------- | ----------------------------------------------------------- | ----------------- | ------------ |
+| 冒泡排序 | [Java](./src/com/scuyjzh/sort/nonlinear/BubbleSort.java)    | *O*(*n*^2)        | *O*(1)       |
+| 插入排序 | [Java](./src/com/scuyjzh/sort/nonlinear/InsertionSort.java) | *O*(*n*^2)        | *O*(1)       |
+| 计数排序 | [Java](./src/com/scuyjzh/sort/linear/CountingSort.java)     | *O*(*n*+*k*)      | *O*(*n*+*k*) |
+| 基数排序 | [Java](./src/com/scuyjzh/sort/linear/RadixSort.java)        | *O*(*d*(*n*+*k*)) | *O*(*n*+*k*) |
+| 桶排序   | [Java](./src/com/scuyjzh/sort/linear/BucketSort.java)       | *O*(*n*)          | *O*(*n*)     |
+
+### Search
+
+| 算法     | 题解                                                | 时间复杂度   | 空间复杂度 |
+| -------- | --------------------------------------------------- | ------------ | ---------- |
+| 二分查找 | [Java](./src/com/scuyjzh/search/BinarySearch.java) | *O*(log *n*) | *O*(1)     |
--- src/com/scuyjzh/sort/linear/BucketSort.java
+++ src/com/scuyjzh/sort/linear/BucketSort.java
@@ -19,7 +19,11 @@ class BucketSort {
      * 以数组作为桶
      */
     public void bucketSort1(int[] arr) {
-        // 判空及防止数组越界
+        /*
+         * 以数组作为桶实现的桶排序，它最大的缺点就是每个桶都和待排序数组一样长，非常消耗内存，容易导致「超出内存限制」错误。
+         * 可以做一个优化：声明时所有的桶都为空，当需要添加数字时，不断扩容，并加入新数字。
+         * 优化之后，以数组作为桶就不会造成太大的内存消耗了，并且不再需要单独采用一个数组来记录每个桶的长度。
+         */
         if (arr == null || arr.length <= 1) {
             return;
         }
@@ -35,7 +39,7 @@ class BucketSort {
         }
         // 确定取值范围
         int range = max - min;
-        // 设置桶的数量，这里设置为 100 个，可以根据实际情况修改。
+        // 设置桶的数量，这里设置为 100 个，可以根据实际情况修改
         int bucketAmount = 100;
         // 桶和桶之间的间距
         double gap = range * 1.0 / (bucketAmount - 1);
@@ -65,6 +69,11 @@ class BucketSort {
      * 数组扩容
      */
     private int[] add(int[] arr, int num) {
+        /*
+         * 这里的扩容算法和 ArrayList 扩容很相似，先开辟一个更长的新数组，并将原数组拷贝过来，再加入新数字。
+         * 但 ArrayList 扩容时，数组长度是先从 0 扩容到 10，后续再不断乘以 1.5 倍，这会造成一定的内存浪费。
+         * 无论是数组还是 ArrayList，扩容过程都比较耗时，所以这个优化属于用时间换空间。
+         */
         if (arr == null) {
             return new int[]{num};
         }
@@ -87,14 +96,26 @@ class BucketSort {
                 j--;
             }
             // 两种情况会跳出循环：
-            // 1. 遇到一个小于或等于 currentNumber 的数字，跳出循环，currentNumber 就坐到它后面。
+            // 1. 遇到一个小于或等于 currentNumber 的数字，跳出循环，currentNumber 就坐到它后面；
             // 2. 已经走到数列头部，仍然没有遇到小于或等于 currentNumber 的数字，也会跳出循环，此时 j 等于 -1，currentNumber 就坐到数列头部。
             arr[j + 1] = currentNumber;
         }
     }
 
+    /**
+     * 以链表作为桶
+     */
     public void bucketSort2(int[] arr) {
-        // 判空及防止数组越界
+        /*
+         * 首先，仍然是找到数组中的最大值和最小值，确定出数据的取值范围，然后划分 100 个桶，计算出间距。
+         * 并且把所有的数字都放入 LinkedList 链表中。装桶后，再对链表进行插入排序即可。
+         *
+         * 采用 LinkedList，装桶时不会有额外的空间浪费，但装桶后排序会比较耗时，
+         * 因为访问 LinkedList 链表时，get 和 set 方法都需要从链表头部开始，逐个向后寻找结点，效率较低。
+         *
+         * 使用链表排序还有一个问题：由于链表中不能存储基本类型，所以不得不将链表类型声明为 LinkedList<Integer>，
+         * int 和 Integer 互相转换的过程被称为 「装箱」和「拆箱」，这也会造成额外的性能消耗。
+         */
         if (arr == null || arr.length <= 1) {
             return;
         }
@@ -110,7 +131,7 @@ class BucketSort {
         }
         // 确定取值范围
         int range = max - min;
-        // 设置桶的数量，这里设置为 100 个，可以任意修改。
+        // 设置桶的数量，这里设置为 100 个，可以任意修改
         int bucketAmount = 100;
         // 桶和桶之间的间距
         double gap = range * 1.0 / (bucketAmount - 1);
@@ -154,15 +175,92 @@ class BucketSort {
                 j--;
             }
             // 两种情况会跳出循环：
-            // 1. 遇到一个小于或等于 currentNumber 的数字，跳出循环，currentNumber 就坐到它后面。
+            // 1. 遇到一个小于或等于 currentNumber 的数字，跳出循环，currentNumber 就坐到它后面；
             // 2. 已经走到数列头部，仍然没有遇到小于或等于 currentNumber 的数字，也会跳出循环，此时 j 等于 -1，currentNumber 就坐到数列头部。
             arr.set(j + 1, currentNumber);
         }
     }
 
+    /**
+     * 折中的方案：装桶时用链表，桶内排序用数组
+     */
+    public void bucketSort3(int[] arr) {
+        /*
+         * 为了解决以上两种数据结构各自的痛点，可以采用一种折中的方案：
+         * 装桶时使用 LinkedList，避免扩容问题，桶内排序时将链表转换为数组，再进行排序，
+         * 避免 LinkedList 排序较慢的问题和大量 「装箱」和「拆箱」的性能消耗（整个链表中的 Integer 都只需要拆箱一次）。
+         *
+         * 在桶内排序前，通过这行代码：
+         * int[] arrInBucket = bucket.stream().mapToInt(Integer::intValue).toArray();
+         * 将 LinkedList 转换为 int[]，然后再进行插入排序。
+         * 代价是这里多了一个中间变量 arrInBucket，它会占用 O(n) 的空间，
+         * 并且 LinkedList 转换为 int[] 的过程需要遍历一次数组，这会增加 O(n) 的时间。
+         */
+        if (arr == null || arr.length <= 1) {
+            return;
+        }
+        // 找到最大值，最小值
+        int max = arr[0];
+        int min = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            } else if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        // 确定取值范围
+        int range = max - min;
+        // 设置桶的数量，这里设置为 100 个，可以任意修改
+        int bucketAmount = 100;
+        // 桶和桶之间的间距
+        double gap = range * 1.0 / (bucketAmount - 1);
+        HashMap<Integer, Queue<Integer>> buckets = new HashMap<>();
+        // 装桶
+        for (int value : arr) {
+            // 找到 value 属于哪个桶
+            int index = (int) ((value - min) / gap);
+            if (!buckets.containsKey(index)) {
+                buckets.put(index, new LinkedList<>());
+            }
+            buckets.get(index).add(value);
+        }
+        int index = 0;
+        // 对每个桶内的数字进行单独排序
+        for (int i = 0; i < bucketAmount; i++) {
+            Queue<Integer> bucket = buckets.get(i);
+            if (bucket == null) {
+                continue;
+            }
+            // 将链表转换为数组，提升排序性能
+            int[] arrInBucket = bucket.stream().mapToInt(Integer::intValue).toArray();
+            // 这里需要结合其他排序算法，例如：插入排序
+            insertSort(arrInBucket);
+            // 排序完成后将桶内的结果收集起来
+            System.arraycopy(arrInBucket, 0, arr, index, arrInBucket.length);
+            index += arrInBucket.length;
+        }
+    }
+
     public static void main(String[] args) {
-        int[] arr = new int[]{1201, 292, 121, 72, 233, 44, 12};
+        /*
+         * 总结一下桶排序算法中，采用各个数据结构作为桶的特点：
+         *   • 以数组作为桶，初始化每个桶的长度为 n：时间上做到了最好，但空间占用很高。
+         *   • 以数组作为桶，初始化每个桶的长度为 0：空间上做到了最好，但装桶时对数组扩容比较耗时。
+         *   • 以 LinkedList 作为桶：空间上做到了最好，并且装桶时无需扩容，但对 LinkedList 排序比较耗时。
+         *   • 装桶时采用 LinkedList，排序时采用数组：时间和空间上都是一种折中的方案，但 LinkedList 转换 int[] 的过程需要遍历一次数组，增加了 O(n)O(n) 的时间，转换后会占用 O(n)O(n) 的空间。
+         */
+        int[] arr;
+        arr = new int[]{1201, 292, 121, 72, 233, 44, 12};
+        new BucketSort().bucketSort1(arr);
+        System.out.println(Arrays.toString(arr));
+
+        arr = new int[]{1201, 292, 121, 72, 233, 44, 12};
         new BucketSort().bucketSort2(arr);
+        System.out.println(Arrays.toString(arr));
+
+        arr = new int[]{1201, 292, 121, 72, 233, 44, 12};
+        new BucketSort().bucketSort3(arr);
         System.out.println(Arrays.toString(arr));
     }
 }
